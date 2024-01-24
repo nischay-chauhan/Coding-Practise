95a1aa666118decf7442eb36cccad6d6c40af51c
nischay-chauhan <masternischay@outlook.com>
2024-01-24 20:28:32 +0530

    Ascending Order

--- hashmaps/map.cpp
+++ hashmaps/map.cpp
@@ -1,23 +1,26 @@
-// Hashmaps COding problem practise to get used to use Hashmaps
-
-/** 1 : You are given an array of integers. Your task is to find the frequency of each distinct element in the array and print them.*/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cout << "Enter the number of ELements : ";    
+    cout << "Enter the number of elements: ";
     cin >> n;
-    unordered_map<int , int> freqMap;
-    for(int i = 0;i<n;i++){
+
+    vector<int> val;
+    unordered_map<int, int> freqMap;
+
+    for (int i = 0; i < n; i++) {
         int num;
-        cout << "Enter the number " << i+1 << " : ";
-        cin>>num;
+        cout << "Enter the number " << i + 1 << ": ";
+        cin >> num;
         freqMap[num]++;
+        val.push_back(num);
     }
 
-    for(auto it : freqMap){
-        cout<<it.first<<" : "<<it.second<<endl;
+    sort(val.begin(), val.end());
+    val.erase(unique(val.begin(), val.end()), val.end()); 
+    for (auto it : val) {
+        cout << it << " : " << freqMap[it] << endl;
     }
 
     return 0;

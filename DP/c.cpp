ccbe03f8421d36ee7198d35bc4bf82584bd2ff78
nischay-chauhan <masternischay@outlook.com>
2025-01-14 00:00:57 +0530

    minPathSum dp

---
+++ DP/c.cpp
@@ -0,0 +1,18 @@
+/* Climb stairs */
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+int main(){
+    int n = 5;
+    cout << climbStairs(n);
+}

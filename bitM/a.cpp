1ff048e4a52f87dd8403b4ca7e21603d6fada92a
nischay-chauhan <masternischay@outlook.com>
2025-09-24 18:45:22 +0530

    bit mani

---
+++ bitM/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 5, b = 3;
+    cout << (a & b) << endl;  // 1
+    cout << (a | b) << endl;  // 7
+    cout << (a ^ b) << endl;  // 6
+    cout << (~a) << endl;     // -6 (2’s complement)
+    cout << (a << 1) << endl; // 10
+    cout << (a >> 1) << endl; // 2
+       
+}
